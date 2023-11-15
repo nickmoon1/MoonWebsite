c05c4bbbf5c311e2c51a4641c506357bc8272197
Nickmoon Mware <nickmoonmware@gmail.com>
2023-11-15 14:16:40 -0600

    Website Configuration

--- components/about.tsx
+++ components/about.tsx
@@ -19,29 +19,27 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        Hello, I'm Moon from Kenya, currently residing in the USA. While pursuing my B.S. degree in {" "}
+        <span className="font-medium">Computer Science</span> at Benedict College, I invested more into my
+        passion for programming. Afterwards, I enrolled in a Masters program at the University of Nebraska-Lincoln (UNL) and learned{" "}
+        <span className="font-medium">the intricacies of Machine Learning and Natural Langauge Processing</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          Python, C++, and SQL
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        . I am also familiar with JavaScript and PHP. I am always looking to
+        learn new technologies. I am currently open to collaborate on any opensource NLP projects.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        video games, playing basketball, swimming and reading. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
+        learning to{" "}
+        <span className="font-medium">cook different cousins </span>. I'm also
         learning how to play the guitar.
       </p>
     </motion.section>
--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+
 
 export const links = [
   {
@@ -35,28 +33,28 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "B.S Degree in Computer Science",
+    location: "Columbia, SC",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "I graduated after 4 years of studying. I immediately applied to Grad school at UNL.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2016 - 2020",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Master's Degree in Computer Science",
+    location: "Lincoln, NE",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "My thesis while pursuing my masters was on the impact of accents on the Operation of Voice User Interfaces(VUIs).",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "2020 - 2022",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "CBTC Integration Engineer",
+    location: "New York, NY",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+      "I worked at Thales, NY on improving and updating train software. Ensured trains met safety protocols for smooth revenue service in NYC.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023 - present",
   },
 ] as const;
 
@@ -66,21 +64,21 @@ export const projectsData = [
     description:
       "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    
   },
   {
     title: "rmtDev",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+  
   },
   {
     title: "Word Analytics",
     description:
       "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    
   },
 ] as const;
 
@@ -88,20 +86,17 @@ export const skillsData = [
   "HTML",
   "CSS",
   "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
+  "PHP",
+  "Python",
+  "C++",
+  "SQL",
   "Node.js",
   "Git",
-  "Tailwind",
-  "Prisma",
-  "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
-  "PostgreSQL",
-  "Python",
-  "Django",
-  "Framer Motion",
+  "Matplotlib",
+  "NumPy",
+  "Librosa",
+  "TensorFlow",
+  "Data Warehouse/ETL",
+  "PLSQL/T-SQL",
+  "Power BI",
 ] as const;
