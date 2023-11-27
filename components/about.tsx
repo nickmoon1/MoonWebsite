85a87dfa42ae19c520ae4223aa2b915662539675
Nickmoon Mware <nickmoonmware@gmail.com>
2023-11-27 12:27:46 -0600

    feat: Add projects and vercel analytics

--- components/about.tsx
+++ components/about.tsx
@@ -22,7 +22,7 @@ export default function About() {
         Hello, I'm Moon from Kenya, currently residing in the USA. While pursuing my B.S. degree in {" "}
         <span className="font-medium">Computer Science</span> at Benedict College, I invested more into my
         passion for programming. Afterwards, I enrolled in a Masters program at the University of Nebraska-Lincoln (UNL) and learned{" "}
-        <span className="font-medium">the intricacies of Machine Learning and Natural Langauge Processing</span>.{" "}
+        <span className="font-medium">the intricacies of Machine Learning and Natural Language Processing</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
@@ -39,7 +39,7 @@ export default function About() {
         video games, playing basketball, swimming and reading. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning to{" "}
-        <span className="font-medium">cook different cousins </span>. I'm also
+        <span className="font-medium">cook different cuisine </span>. I'm also
         learning how to play the guitar.
       </p>
     </motion.section>
