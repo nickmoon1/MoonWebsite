9f3fe5ea0197496ce54696180badc4661a067ac3
Nickmoon Mware <nickmoonmware@gmail.com>
2023-11-30 13:45:48 -0600

    fix: Fixed Analytics Errors

---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Moon Mware on 11/30/23.
+//
+
+import Foundation
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import About from "@/components/about";
 import Contact from "@/components/contact";
-import {Analytics} from "@vercel/analytics/react";
+import Analytics from "@vercel/analytics";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
